991300768f7eb5a48bd985a783e2628be0646265
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-10-18 13:27:55 +0300

    new version dm

--- scripts/dm_TEST_erp_sales_v2.sql
+++ scripts/dm_TEST_erp_sales_v2.sql
@@ -12,7 +12,12 @@ WITH
 			COUNT(*) OVER my_window
 		FROM sttgaz.dds_isc_nomenclature_guide n
 		WHERE n.Дивизион IN ('LCV', 'MCV', 'BUS')
-		WINDOW my_window AS (PARTITION BY n."Модель на заводе", n.Производитель, n.Дивизион)
+		WINDOW my_window AS (
+			PARTITION BY 
+			n."Модель на заводе", 
+			n.Производитель 
+			-- n.Дивизион    ------- Убрать, если появятся дубли и перестанут проходиться проверки.
+		)
 	),
 	nom AS(
 		SELECT DISTINCT
@@ -23,7 +28,12 @@ WITH
 			Дивизион
 		FROM nom_prep
 		WHERE Код65 IS NOT NULL OR count = 1
-		WINDOW my_window AS (PARTITION BY "Модель на заводе", Производитель, Дивизион ORDER BY Ид DESC)
+		WINDOW my_window AS (
+			PARTITION BY 
+			"Модель на заводе",
+			Производитель
+			-- Дивизион	   ------- Убрать, если появятся дубли и перестанут проходиться проверки.
+			ORDER BY Ид DESC)
 	),
 	erp_nom_join AS(
 		SELECT
@@ -39,15 +49,15 @@ WITH
 			ON s.Контрагент = n.Производитель
 				AND(
 					"Чертежный номер комплекта" = n."Модель на заводе"
-					OR REPLACE(s."Чертежный номер комплекта", '-00', '-') = REGEXP_REPLACE(n.Код65 , '^А', 'A')
-					OR REPLACE(s."Чертежный номер комплекта", '-00', '-') = REGEXP_REPLACE(n.Код65 , '^С', 'C')
+					-- OR REPLACE(s."Чертежный номер комплекта", '-00', '-') = REGEXP_REPLACE(n.Код65 , '^А', 'A')	    ------- Убрать, если появятся дубли и перестанут проходиться проверки.
+					-- OR REPLACE(s."Чертежный номер комплекта", '-00', '-') = REGEXP_REPLACE(n.Код65 , '^С', 'C')		------- Убрать, если появятся дубли и перестанут проходиться проверки.
 				)
 		LEFT JOIN nom																AS n2
 			ON n."Модель на заводе" IS NULL AND n2.Производитель ILIKE '%ГАЗ ПАО%'
 				AND(
 					s."Чертежный номер комплекта" = n2."Модель на заводе"
-					OR REPLACE(s."Чертежный номер комплекта", '-00', '-') = REGEXP_REPLACE(n2.Код65 , '^А', 'A')
-					OR REPLACE(s."Чертежный номер комплекта", '-00', '-') = REGEXP_REPLACE(n2.Код65 , '^С', 'C')
+					-- OR REPLACE(s."Чертежный номер комплекта", '-00', '-') = REGEXP_REPLACE(n2.Код65 , '^А', 'A')   ------- Убрать, если появятся дубли и перестанут проходиться проверки.
+					-- OR REPLACE(s."Чертежный номер комплекта", '-00', '-') = REGEXP_REPLACE(n2.Код65 , '^С', 'C')   ------- Убрать, если появятся дубли и перестанут проходиться проверки.
 				)
 	),
 	erp_isc_union AS(
@@ -93,16 +103,63 @@ WITH
 					p.ТоварКод65,
 					p."Вариант сборки",
 					d."Направление реализации с учетом УКП"
+	),
+	calendar AS(
+		SELECT DISTINCT DATE_TRUNC('MONTH', ts)::date AS "Месяц"
+		FROM (SELECT '2016-01-01'::TIMESTAMP as tm 
+			  UNION ALL
+			  SELECT NOW() ) as t
+		TIMESERIES ts as '1 day' OVER (ORDER BY t.tm)
+	),
+	units AS(
+		SELECT DISTINCT
+			"Дивизион",
+			"Внутренний код",
+			ТоварКод65,
+			"Вариант сборки",
+			"Направление реализации с учетом УКП" 
+		FROM erp_isc_union
+	),
+	matrix AS(
+		SELECT *
+		FROM calendar
+		CROSS JOIN units
+	),
+	add_windows AS (
+		SELECT
+			m."Месяц",
+			m."Дивизион",
+			m."Внутренний код",
+			m.ТоварКод65,
+			m."Вариант сборки",
+			m."Направление реализации с учетом УКП" ,
+			s."Реализовано",
+			SUM("Реализовано") OVER (
+				PARTITION BY m.Дивизион, m."Внутренний код", m.ТоварКод65, m."Вариант сборки", m."Направление реализации с учетом УКП" 
+				ORDER BY m.Месяц
+			)		 																										AS "Продано с накоплением",
+			'Автокомплекты' 																								AS "Тип продукции"
+		FROM matrix																											AS m
+		LEFT JOIN erp_isc_union																								AS s
+			ON m."Месяц" = s."Месяц"
+			AND HASH(
+				m."Дивизион",
+				m."Внутренний код",
+				m.ТоварКод65,
+				m."Вариант сборки",
+				m."Направление реализации с учетом УКП" 
+			) = HASH(
+				s."Дивизион",
+				s."Внутренний код",
+				s.ТоварКод65,
+				s."Вариант сборки",
+				s."Направление реализации с учетом УКП" 		
+			)
 	)
-SELECT
-	*,
-	SUM("Реализовано") OVER (
-		PARTITION BY Дивизион, "Внутренний код", ТоварКод65, "Вариант сборки",
-		"Направление реализации с учетом УКП" 
-		ORDER BY Месяц
-	)		 																										AS "Продано с накоплением",
-	'Автокомплекты' 																								AS "Тип продукции"
-FROM erp_isc_union;
+SELECT *
+FROM add_windows
+WHERE "Реализовано" IS NOT NULL 
+	OR "Продано с накоплением" IS NOT NULL;
 
 GRANT SELECT ON TABLE sttgaz.dm_TEST_erp_sales_v2 TO PowerBI_Integration WITH GRANT OPTION;
 COMMENT ON TABLE sttgaz.dm_TEST_erp_sales_v2 IS 'Продажи автокомплектов из ERP';
--- scripts/dm_TEST_isc_sales_v2.sql
+++ scripts/dm_TEST_isc_sales_v2.sql
@@ -53,14 +53,61 @@ WITH
             s."Вариант сборки",
 			s."Направление реализации с учетом УКП",
 			"Тип продукции"
+	),
+	calendar AS(
+		SELECT DISTINCT DATE_TRUNC('MONTH', ts)::date AS "Месяц"
+		FROM (SELECT '2016-01-01'::TIMESTAMP as tm 
+			  UNION ALL
+			  SELECT NOW() ) as t
+		TIMESERIES ts as '1 day' OVER (ORDER BY t.tm)
+	),
+	units AS(
+		SELECT DISTINCT
+			"Дивизион",
+			"Внутренний код",
+			Код65,
+			"Вариант сборки",
+			"Направление реализации с учетом УКП" 
+		FROM sales_agregate
+	),
+	matrix AS(
+		SELECT *
+		FROM calendar
+		CROSS JOIN units
+	),
+	add_windows AS (	
+		SELECT
+			m."Месяц",
+			m."Дивизион",
+			m."Внутренний код",
+			m.Код65,
+			m."Вариант сборки",
+			m."Направление реализации с учетом УКП" ,
+			s."Продано в розницу",
+			SUM("Продано в розницу") OVER (
+				PARTITION BY  m.Дивизион, m."Внутренний код", m.Код65, m."Вариант сборки", m."Направление реализации с учетом УКП" 
+				ORDER BY m.Месяц)	 																							AS "Продано с накоплением"    
+			FROM matrix																											AS m
+			LEFT JOIN sales_agregate																							AS s
+				ON m."Месяц" = s."Месяц"
+				AND HASH(
+						m."Дивизион",
+						m."Внутренний код",
+						m.Код65,
+						m."Вариант сборки",
+						m."Направление реализации с учетом УКП" 
+				) = HASH(
+						s."Дивизион",
+						s."Внутренний код",
+						s.Код65,
+						s."Вариант сборки",
+						s."Направление реализации с учетом УКП" 		
+				)
 	)
-SELECT
-	*,
-	SUM("Продано в розницу") OVER (
-		PARTITION BY Дивизион, "Внутренний код", Код65, "Вариант сборки",
-		"Направление реализации с учетом УКП" 
-		ORDER BY Месяц)	 AS "Продано с накоплением"    
-FROM sales_agregate;
+SELECT *
+FROM add_windows
+WHERE "Продано в розницу" IS NOT NULL 
+	OR "Продано с накоплением" IS NOT NULL;
 
 GRANT SELECT ON TABLE sttgaz.dm_TEST_isc_sales_v2 TO PowerBI_Integration WITH GRANT OPTION;
 COMMENT ON TABLE sttgaz.dm_TEST_isc_sales_v2 IS 'Продажи ТС из ИСК';
